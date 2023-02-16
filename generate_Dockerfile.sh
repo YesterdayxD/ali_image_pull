dfd2a55d9f2b4963ef9900b3acc954c767285a08
YesterdayxD <1144650984@qq.com>
2023-02-16 17:08:27 +0800

    add

--- generate_Dockerfile.sh
+++ generate_Dockerfile.sh
@@ -2,9 +2,9 @@ images_list=$(cat images_list)
 echo ${images_list}
 for i in ${images_list};do
 	path_=$(echo ${i}|cut -d \: -f 1)
-	tag=$(echo ${i}|cut -d \: -f 2)
+	tag=$(echo ${i}|cut -d \: -f 2|cut -d \< -f 1)
 	echo ${path_}
 	echo ${tag}
 	mkdir -p ${path_}
-	echo "from ${i}" > ${path_}/Dockerfile.${tag}
+	echo "from ${path_}:${tag}" > ${path_}/Dockerfile.${tag}
 done
---
+++ tag.sh
@@ -0,0 +1,20 @@
+images_list=$(cat images_list)
+echo ${images_list}
+for i in ${images_list};do
+	part1=$(echo ${i}|cut -d \< -f 1)
+	part2=$(echo ${i}|cut -d \< -f 2)
+	echo ${part1}
+	echo ${part2}
+
+	path_=$(echo ${part2}|cut -d \: -f 1)
+	tag=$(echo ${part2}|cut -d \: -f 2)
+
+	# 拉取
+	docker pull ${part2}
+	# tag
+	docker tag ${part2} ${part1}
+
+	# save
+	docker save ${part1} > ${tag}.tar
+
+done
